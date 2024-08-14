2e545f7d39b66cf858e389ad3dfb1bc122ecfc2d
Quang Vuong <quang@coreclinicalsciences.com>
2024-08-14 13:05:03 -0700

    Adjusted GC vignette

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/articles/special-files.html
 
 library(testthat)
-library(TransportHealthR)
+library(TransportHealth)
 
-test_check("TransportHealthR")
+test_check("TransportHealth")
